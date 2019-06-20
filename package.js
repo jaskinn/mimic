ca11f0d047c81e0e97eaf1bf7a1d6e447f84ecb8
Jake <jake@zuusworkforce.com>
2019-06-20 16:30:08 -0600

    Update package name

--- package.js
+++ package.js
@@ -2,37 +2,34 @@
 /* eslint-disable prefer-arrow-callback */
 
 Package.describe({
-  name: 'hwillson:stub-collections',
-  version: '1.0.9',
-  summary: 'Stub out Meteor collections with in-memory local collections.',
-  documentation: 'README.md',
-  git: 'https://github.com/hwillson/meteor-stub-collections.git',
-  debugOnly: true,
-});
+    name: 'jaskinn:mimic',
+    version: '1.0.9',
+    summary:
+        'Stub out Meteor collections with in-memory local collections. (forked from hwillson:meteor-stub-collections)',
+    documentation: 'README.md',
+    git: 'https://github.com/jaskinn/mimic.git',
+    debugOnly: true,
+})
 
 Npm.depends({
-  chai: '4.1.2',
-  sinon: '6.0.1',
-});
+    chai: '4.2.0',
+    sinon: '7.3.2',
+})
 
 Package.onUse(function onUse(api) {
-  api.versionsFrom('1.3');
-  api.use([
-    'ecmascript',
-    'mongo',
-  ]);
-  api.mainModule('index.js');
-});
+    api.versionsFrom('1.8')
+    api.use(['ecmascript', 'mongo'])
+    api.mainModule('index.js')
+})
 
 Package.onTest(function onTest(api) {
-  api.use([
-    'hwillson:stub-collections',
-    'aldeed:simple-schema@1.5.3',
-    'aldeed:collection2@2.10.0',
-    'ecmascript',
-    'mongo',
-    'lmieulet:meteor-coverage@2.0.2', // Needed until https://github.com/meteortesting/meteor-mocha/pull/69 is merged
-    'meteortesting:mocha@1.0.0',
-  ]);
-  api.mainModule('tests.js');
-});
+    api.use([
+        'jaskinn:mimic',
+        'aldeed:collection2@3.0.1',
+        'ecmascript',
+        'mongo',
+        'lmieulet:meteor-coverage@3.2.0',
+        'meteortesting:mocha@1.1.2',
+    ])
+    api.mainModule('tests.js')
+})
