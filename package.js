b4314981e8b8dd072271709d0062f84c9ba190b1
Jake <jake@zuusworkforce.com>
2020-01-10 13:58:00 -0700

    Update dependancies

--- package.js
+++ package.js
@@ -2,34 +2,35 @@
 /* eslint-disable prefer-arrow-callback */
 
 Package.describe({
-    name: 'jaskinn:mimic',
-    version: '1.1.0',
-    summary: 'Stub out Meteor collections (forked from hwillson:meteor-stub-collections)',
-    documentation: 'README.md',
-    git: 'https://github.com/jaskinn/mimic.git',
-    debugOnly: true,
-})
+    name: "jaskinn:mimic",
+    version: "1.1.0",
+    summary:
+        "Stub out Meteor collections (forked from hwillson:meteor-stub-collections)",
+    documentation: "README.md",
+    git: "https://github.com/jaskinn/mimic.git",
+    debugOnly: true
+});
 
 Npm.depends({
-    chai: '4.2.0',
-    sinon: '7.3.2',
-})
+    chai: "4.2.0",
+    sinon: "8.0.4",
+    "simpl-schema": "1.5.3"
+});
 
 Package.onUse(function onUse(api) {
-    api.versionsFrom('1.8')
-    api.use(['ecmascript', 'mongo'])
-    api.mainModule('index.js')
-})
+    api.versionsFrom("1.8");
+    api.use(["ecmascript", "mongo"]);
+    api.mainModule("index.js");
+});
 
 Package.onTest(function onTest(api) {
     api.use([
-        'jaskinn:mimic',
-        'aldeed:collection2@3.0.1',
-        'ecmascript',
-        'mongo',
-        'lmieulet:meteor-coverage@3.2.0',
-        'meteortesting:mocha@1.1.2',
-        'matb33:collection-hooks@0.8.4',
-    ])
-    api.mainModule('tests.js')
-})
+        "jaskinn:mimic",
+        "aldeed:collection2@3.0.6",
+        "ecmascript",
+        "mongo",
+        "meteortesting:mocha@1.1.3",
+        "matb33:collection-hooks@0.9.1"
+    ]);
+    api.mainModule("tests.js");
+});
